a3c8a259843875f4985903126e177066fd08da96
Behzad Dogahe <bdogahe@us.ibm.com>
2019-11-22 16:37:03 -0500

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -4,12 +4,13 @@ mongoose.set('useCreateIndex', true);
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 connect.then((db) => {
 
     console.log('Connected correctly to server');
 
+    /*
     var newDish = Dishes({
         name: 'Uthappizza',
         description: 'test'
@@ -23,6 +24,66 @@ connect.then((db) => {
         })
         .then((dishes) => {
             console.log(dishes);
+
+            return Dishes.deleteMany({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+
+    Dishes.create({
+        name: 'Sourdough Bread',
+        description: 'A bread made out of a sourdough.'
+    })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.deleteMany({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        */
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated test' }
+            }, {
+                new: true
+            })
+                .exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
 
             return Dishes.deleteMany({});
         })
